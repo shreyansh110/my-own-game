9d54c26fabd5d78a945ac8f09bccbfd0ead67a92
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2021-08-22 13:44:00 +0530

    Add files via upload

---
+++ my own game/theif.js
@@ -0,0 +1,26 @@
+class Theif{
+    constructor(x,y,width,height){
+    
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    
+        var options={
+    
+            'restitution':0.8,
+            'friction':1.2,
+            'density':1.2,
+            isStatic:true
+        }
+    
+        this.body=createSprite(this.x,this.y,this.width,this.height,options);
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
