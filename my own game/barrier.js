25c97ee4e36d022a6d5d13625bea79892982aafa
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2021-09-09 20:58:20 +0530

    Add files via upload

---
+++ my own game/barrier.js
@@ -0,0 +1,21 @@
+class Barrier{
+    constructor(x,y,width,height){
+    
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+
+    
+
+    
+        this.body=createSprite(this.x,this.y,this.width,this.height);
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
---
+++ my own game/gem.js
@@ -0,0 +1,20 @@
+class Gem{
+    constructor(x,y,width,height){
+    
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    
+
+    
+        this.body=createSprite(this.x,this.y,this.width,this.height);
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
