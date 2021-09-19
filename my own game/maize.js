1fc645a44d99b475089d6cc83e5549de924f7c38
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2021-09-19 21:56:48 +0530

    Add files via upload

---
+++ my own game/maize.js
@@ -0,0 +1,20 @@
+class Maize{
+constructor(x,y,width,height){
+
+this.x=x;
+this.y=y;
+this.width=width;
+this.height=height;
+
+
+
+    this.body=createSprite(this.x,this.y,this.width,this.height);
+
+}
+
+
+
+
+
+
+}
